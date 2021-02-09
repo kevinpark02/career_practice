a30eb223ce68da742c57d82d728640a388cdab51
kevin park <kevin.park02@gmail.com>
2021-02-09 13:24:27 -0800

    Solve two more problems from open resources

--- leetcode_problems/pair_board.rb
+++ leetcode_problems/pair_board.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # 01A
 
 # Digital Root
@@ -38,3 +39,37 @@ def caesar_cipher(msg, increment)
 end
 
 # 01B
+
+# longest_common_substring
+
+def longest_common_substring(str1, str2)
+    substrings = []
+    longest = ""
+
+    str1.each_char.with_index do |char1, idx1|
+        str1.each_char.with_index do |char2, idx2|
+            if idx2 > idx1
+                substrings << str1[idx1..idx2]
+            end
+        end
+    end
+
+    substrings.each do |substring|
+        if str2.include?(substring) && (substring.length > longest.length)
+            longest = substring
+        end
+    end
+
+    longest
+
+end
+
+# sum_rec
+
+def sum_rec(nums)
+    return 0 if nums.length == 0
+
+    return nums[0] + sum_rec(nums[1..-1])
+end
+
+# 
